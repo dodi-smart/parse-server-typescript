e7828317dd06ece00c9282ad933594f30df1719b
Asen Lekov <asenlekoff@gmail.com>
2021-10-11 00:08:51 +0300

    Add utilities

---
+++ src/cloud/utils/versioned.ts
@@ -0,0 +1,14 @@
+export type VersionedFunction = (req: Parse.Cloud.JobRequest | Parse.Cloud.FunctionRequest) => Promise<void>;
+
+// This function will run the functions based on the version param
+// If the version param is set, will lookup the function and run it
+// otherwise will run the default
+export function versioned(implementations: { [x: string]: VersionedFunction; default: VersionedFunction }) {
+    return function (req: Parse.Cloud.JobRequest | Parse.Cloud.FunctionRequest): Promise<void> {
+        const { version } = req.params
+        const impl = implementations[version] || implementations.default
+        return impl(req)
+    }
+}
+
+
